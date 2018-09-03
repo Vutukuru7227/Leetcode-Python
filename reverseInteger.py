a5ee4d801d9068d75787cb051a8608da0c437d06
Ram Anand Vutukuru <anand.vootukuru@gmail.com>
2018-09-02 18:21:41 -0700

    Reverse Integer solution updated

--- reverseInteger.py
+++ reverseInteger.py
@@ -1,18 +1,17 @@
 class Solution(object):
-    def reverse(self, x):
-        input_to_str = str(x)
-        input_sign = str[0]
-        input_str_int =  str[1:]
-        str_int_reverse = input_str_int[::-1]
-        result_string = input_sign+""+str_int_reverse
-        result = int(result_string)
-        print(result)
+    @staticmethod
+    def reverse(x):
+        sign = str(x)[0]
+        if sign != "-":
+            result = int(str(x)[::-1])
+        else:
+            output = sign+""+str(int(str(x)[:0:-1]))
+            result = int(output)
 
+        if result < 2147483648 and result > -2147483648:
+            return result
+        else:
+            return 0
+        return result
 
-def main():
-    sln = Solution
-    sln.reverse(-123)
-
-
-if __name__ == '__main__':
-    main()
+Solution.reverse(120)
